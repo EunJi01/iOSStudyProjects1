8f72600cdca90fbb80b12cfed8e5452d4d2af9a4
EunJi <hej0102@naver.com>
2022-04-01 00:24:13 +0900

    fix: code fix

--- AutoLayoutExample/AutoLayoutExample/ViewController.swift
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -16,9 +16,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapChangeColorButton(_ sender: UIButton) {
-        self.colorView.backgroundColor = UIColor.blue
+        self.colorView.backgroundColor = getRandomColor()
         print("색상 변경 버튼이 클릭되었음")
     }
     
+    func getRandomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+            
+    }
 }
 
